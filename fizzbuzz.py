34f77644b21d6fb23888eea40f5b6216cbdf0797
wata-pom <w.kumagai1224@gmail.com>
2019-11-03 11:32:38 +0900

    fizzbuzz問題

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,14 @@
 def fizzbuzz_convert(number):
+    if number % 15 == 0:
+        return 'FizzBuzz'
     if number % 3 == 0:
         return 'Fizz'
+    if number % 5 == 0:
+        return 'Buzz'
+    return number
 
 
 assert fizzbuzz_convert(3) == 'Fizz'
+assert fizzbuzz_convert(6) == 'Fizz'
+assert fizzbuzz_convert(5) == 'Buzz'
+assert fizzbuzz_convert(15) == 'FizzBuzz'
